fa22cce8cc34628a822903d6197c3da8f93e5205
Joris <dehoogjoris@gmail.com>
2018-12-05 21:17:30 +0100

    not ready yet, i think the iteration is not right

--- day 1/day1PT2.py
+++ day 1/day1PT2.py
@@ -4,19 +4,29 @@
 # it may be that the input list is looped over many times
 
 # let's test first: 
-data = [3, 3, 4, -2, -4]
+# data = [+7, +7, -2, -7, -4]
+
+import itertools
 
 freq = 0
 freqList = []
 working = True
-while working == True:
-    # loop over items
-    for item in data:
-        freq += int(item)
-        if freq in freqList:
-            print(freq)
-            # False
-            working = False
-        else:
-            freqList.append(freq)
-            working = True
+# i is the loop index
+i = 0
+
+with open(r'day 1\input', 'r', newline='\r\n') as infile:
+    # data = infile.read().splitlines()
+    data = [+7, +7, -2, -7, -4]
+    # do the thing
+    while working == True:
+        for item in data:
+            freq += int(item)
+            if freq in freqList:
+                working = False
+                break
+            else:
+                freqList.append(freq)
+                working = True
+
+    print(freq)
+    print(freqList)
