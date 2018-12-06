2278413dccbf9abe7478103ea4c9a46378513229
Joris <dehoogjoris@gmail.com>
2018-12-06 18:45:40 +0100

    I did it mom, it finishes in around 80 seconds though

--- day 1/day1PT2.py
+++ day 1/day1PT2.py
@@ -3,54 +3,32 @@
 # while looping trough the list, find the first frequency that is repeated. 
 # it may be that the input list is looped over many times
 
-# let's test first: 
-# data = [+7, +7, -2, -7, -4]
+# it also seems that this solution takes around 78 seconds on my machine to complete...
 
-import itertools
+import time
+
+# start timer
+startTime = time.time()
 
 infile = open(r'day 1\input', 'r', newline='\r\n')
 
 # definitions
 freq = 0
-loops = 0
 freqList = []
-elements = []
 working = True
-# i is the loop index
-i = 0
 
-# I want to know how many loops are done, so here's a function
-def findSublist(a, b, l):
-    if len(a) > len(b):
-        return False
-    # if l == 0: 
-    for i in range(0, len(b) - len(a) + 1):
-        print(b[i: i + len(a)-1])
-        if b[i: i + len(a)] == a:
-            return True
-    # else: 
-    #     for i in range(l, )
-        
-    return False
-
-# data = infile.read().splitlines()
-data = [+2, +2, -1, -7, -4, -3, 5 , -1, -11]
+data = infile.read().splitlines()
 # do the thing
 while working == True:
     for item in data:
-        elements.append(item)
+        item = int(item)
         freq += item
         if freq in freqList:
             working = False
             break
         else:
             freqList.append(freq)
-            if findSublist(data, elements, loops):
-                loops += 1
-                print("Looped {} time(s)".format(str(loops)))
             working = True
 
 print('Answer found: {}'.format(freq))
-print(freqList)
-# print(data)
-print(elements)
+print('Execution time: {} seconds'.format(time.time() - startTime))
