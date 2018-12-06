fecb90a0ef382cda4c93aaa414380cfd21841a75
Joris <dehoogjoris@gmail.com>
2018-12-06 23:12:38 +0100

    Cleaned up and ready to go

--- day 2/day2PT2.py
+++ day 2/day2PT2.py
@@ -1,26 +1,6 @@
 # the input data contains a bunch of strings. We need to find the two strings that differ in exactly 
 # one character on the exact same place. Then we need to find the corresponding letters
 
-data = [
-    "abcde",
-    "fghij",
-    "klmno",
-    "pqrst",
-    "fguij",
-    "axcye",
-    "wvxyz"
-]
-
-# test something
-# a = [1, 2, 3, 4, 5]
-# b = [1, 2, 9, 8, 5]
-# total = []
-# for x, y in zip(a, b):
-#     total.append(x - y)
-
-# # remove the zeros
-# total = [p for p in total if p != 0]
-# print(len(total))
 
 # loop over list, compare all following items to the considered file
 # for each item, transform it into a list of letters, keeping the order
@@ -30,20 +10,18 @@ data = [
 infile = open(r'day 2\input', 'r', newline='\r\n')
 data = infile.read().splitlines()
 
-print('length of the input array: {}'.format(len(data)))
-
 for i in range(0, len(data)):
     itemList = list(data[i])
+
     # look im doing list comprehension!
     itemListOrd = [ord(p)-96 for p in itemList]
-    # print('-- {} --'.format(itemList))
 
-    # print(itemList)
     # look for all following items
     for j in range(i+1, len(data)):
         itemCheck = list(data[j])
+        # convert to numbers so we can subtract these lists from each other to find the 'pattern'
         itemCheckOrd = [ord(p)-96 for p in itemCheck]
-        # use zip to loop over the two lists?
+        # subtract both. If the list of unique items in total is exactly one item long, we have found our match 
         total = []
         for x, y in zip(itemListOrd, itemCheckOrd):
             total.append(x-y)
@@ -58,10 +36,6 @@ for i in range(0, len(data)):
             print('we found it!')
             print('Answer: {}'.format(''.join(itemList)))
 
-
-
-        
-        
-        # find what positions match the itemList
-
-
+# Thoughts: I don't stop the loop when the match is found.
+# Let's say it's simply to check wether or not we have a good input set
+# and not accidentaly more than one match :)
