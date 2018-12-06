72969a4b76a266abfe2a5fd16d4f9710ced78abe
Joris <dehoogjoris@gmail.com>
2018-12-06 18:07:12 +0100

    what am i doing

--- day 1/day1PT2.py
+++ day 1/day1PT2.py
@@ -8,25 +8,49 @@
 
 import itertools
 
+infile = open(r'day 1\input', 'r', newline='\r\n')
+
+# definitions
 freq = 0
+loops = 0
 freqList = []
+elements = []
 working = True
 # i is the loop index
 i = 0
 
-with open(r'day 1\input', 'r', newline='\r\n') as infile:
-    # data = infile.read().splitlines()
-    data = [+7, +7, -2, -7, -4]
-    # do the thing
-    while working == True:
-        for item in data:
-            freq += int(item)
-            if freq in freqList:
-                working = False
-                break
-            else:
-                freqList.append(freq)
-                working = True
+# I want to know how many loops are done, so here's a function
+def findSublist(a, b, l):
+    if len(a) > len(b):
+        return False
+    # if l == 0: 
+    for i in range(0, len(b) - len(a) + 1):
+        print(b[i: i + len(a)-1])
+        if b[i: i + len(a)] == a:
+            return True
+    # else: 
+    #     for i in range(l, )
+        
+    return False
 
-    print(freq)
-    print(freqList)
+# data = infile.read().splitlines()
+data = [+2, +2, -1, -7, -4, -3, 5 , -1, -11]
+# do the thing
+while working == True:
+    for item in data:
+        elements.append(item)
+        freq += item
+        if freq in freqList:
+            working = False
+            break
+        else:
+            freqList.append(freq)
+            if findSublist(data, elements, loops):
+                loops += 1
+                print("Looped {} time(s)".format(str(loops)))
+            working = True
+
+print('Answer found: {}'.format(freq))
+print(freqList)
+# print(data)
+print(elements)
