d0d61e414fa0b8bd626b73445c635de2722c6236
Joris de Hoog <RD5766@external.engie.com>
2018-12-07 15:00:44 +0100

    initial commit

---
+++ day 3/day3PT1.py
@@ -0,0 +1,4 @@
+# canvas of at least 1000X1000. Every elf claims a square of a certain width and height, and coordinates for the top-left starting point
+# we need to figure out how many 'squares' are overlapping when all claims are used
+
+ 
