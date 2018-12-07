d2613e8492e4b34a4fa4e189f186e585f48c7033
Joris de Hoog <RD5766@external.engie.com>
2018-12-07 16:38:57 +0100

    initial commit of pt2

---
+++ day 3/day3PT2.py
@@ -0,0 +1,48 @@
+# canvas of at least 1000X1000. Every elf claims a square of a certain width and height, and coordinates for the top-left starting point
+# we need to figure out how many 'squares' are overlapping when all claims are used
+
+# import statements
+import re
+import numpy as np
+
+infile = open(r'day 3\input', 'r', newline='\r\n')
+data = infile.read().splitlines()
+
+# the canvas is at least 1000*1000 large, but it could be larger? Looking at the input data it seems to not exceed this limit
+canvas = np.zeros((1000, 1000))
+
+def getInfoFromInput(str):
+    # get the id
+    # two steps: first get the pound + following number
+    idFirst = re.search(r'#[0-9]*', str)
+    # second step: get everything behind the pound sign (alternatively, get everything that is not a pound sign)
+    idClaim = re.search(r'[^#][0-9]*', idFirst.group())
+    idClaim = int(idClaim.group())
+
+    # this gets the starting positions
+    startPos = re.search(r'\d*\,\d*', str)
+    startPos = startPos.group()
+    startPos = startPos.split(',')
+    startPos = [int(i) for i in startPos]
+
+    # get the size of each canvas
+    canvasSize = re.search(r'\d*x\d*', str)
+    canvasSize = canvasSize.group()
+    canvasSize = canvasSize.split('x')
+    canvasSize = [int(i) for i in canvasSize]
+
+    # return everything
+    return idClaim, startPos, canvasSize
+
+
+for element in data:
+    idClaim, startPos, claimSize = getInfoFromInput(element)
+    # start populating the canvas
+    for i in range(0, claimSize[0]):
+        for j in range(0, claimSize[1]):
+            canvas[startPos[1] + j][startPos[0] + i] += 1
+    
+overlap = np.where(canvas >= 2)
+
+print('Overlapping squares: {}'.format(len(overlap[0])))
+
