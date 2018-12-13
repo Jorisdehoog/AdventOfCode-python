aeaaa824cfa6ddd7693ef1d262c7f156f8d8413a
Joris <dehoogjoris@gmail.com>
2018-12-13 22:19:52 +0100

    all clean and ready to go

--- day 4/day4PT1.py
+++ day 4/day4PT1.py
@@ -1,31 +1,8 @@
-# minute at asleep = this minute is sleeping
-# minute at await = this minute is awake
-import pprint
 import re
 from dateutil import parser
-from operator import itemgetter
 from collections import defaultdict
 from collections import Counter
 
-# data = [
-#     "[1518-11-01 00:00] Guard #10 begins shift",
-#     "[1518-11-01 00:05] falls asleep",
-#     "[1518-11-01 00:25] wakes up",
-#     "[1518-11-01 00:30] falls asleep",
-#     "[1518-11-01 00:55] wakes up",
-#     "[1518-11-01 23:58] Guard #99 begins shift",
-#     "[1518-11-02 00:40] falls asleep",
-#     "[1518-11-02 00:50] wakes up",
-#     "[1518-11-03 00:05] Guard #10 begins shift",
-#     "[1518-11-03 00:24] falls asleep",
-#     "[1518-11-03 00:29] wakes up",
-#     "[1518-11-04 00:02] Guard #99 begins shift",
-#     "[1518-11-04 00:36] falls asleep",
-#     "[1518-11-04 00:46] wakes up",
-#     "[1518-11-05 00:03] Guard #99 begins shift",
-#     "[1518-11-05 00:45] falls asleep",
-#     "[1518-11-05 00:55] wakes up"
-# ]
 
 infile = open(r'day 4\input', 'r', newline='\r\n')
 data = infile.read().splitlines()
@@ -47,57 +24,32 @@ def inputParser(elem):
         # print("Guard: {}".format(guard))
     else:
         guard = None
-    
-    # get the state of the guard
-    awoken = False
-    awake = False
-    if 'wakes' in elem:
-        awoken = True
-        awake = True
 
-    if 'begins' in elem:
-        awake = True
-    
-    return date, message , guard, awoken, awake
+    return date, message , guard
 
-
-pp = pprint.PrettyPrinter()
 # main logic
 if __name__ == '__main__':
     sortedList = sorted(data)
     # loop over the input and parse the elements
     # keep a list of guards, asleep times
-    # i need to keep in mind that we look for the time a guard will likely be asleep
-    # should i use another data structure then dicts? 
     guardsleeping = defaultdict(list)
     guardminutes = defaultdict(list)
 
-    schedule = {}
-
-    # schedule = defaultdict(int)
     # keep track of current guard
     currentGuard = None
     for item in sortedList:
-        date, msg, guard, awoken, awake = inputParser(item)
-        print(item)
+        date, msg, guard = inputParser(item)
 
         if guard:
             currentGuard = guard
         if currentGuard not in guardsleeping:
             guardsleeping[currentGuard] = 0
         if 'falls' in msg:
-            # print('fell asleep')
             sleeptime = date
         if 'wakes' in msg:
-            # should always pass
-            if sleeptime:
-                sleepdelta = date - sleeptime
-            else:
-                print('what')
-            # print(sleepdelta.seconds/60)
+            sleepdelta = date - sleeptime
             guardsleeping[currentGuard] += sleepdelta.seconds/60
             # keep track of the minutes the guard was sleeping
-            # print(range(sleeptime.minute, date.minute))
             guardminutes[currentGuard].append(list(range(sleeptime.minute, date.minute+1)))
 
     
@@ -109,7 +61,6 @@ if __name__ == '__main__':
     allminutes = guardminutes[maxi]
     # flatten this list
     allmin = [item for sublist in allminutes for item in sublist]
-    # print(allminutesunion)
 
     # use the counter to get the first most common item
     mostsleptmin, mostsleptnum = Counter(allmin).most_common(1)[0]
