ffca7336a8fdf6ef42bbebf610d5bb0d3b776edc
Joris de Hoog <RD5766@external.engie.com>
2018-12-21 13:24:15 +0100

    new commit bc i did not read the question fully and need to remove a bunch of stuff

--- day 6/day6PT2.py
+++ day 6/day6PT2.py
@@ -42,10 +42,62 @@ for item in data:
     grid[y][x] = (i)
     i += 1
 
+# WAIT no i misread the question: I need to loop over all the points in the grid, and if the accumulative distances to the other points are
+# less than 32, mark it. 
+
+
+
+
+# remark: i still need to make the list of all valid areas using the solution for pt1
+# i can probably make this more efficient though, i botched that one pretty badly
+
+# print(grid)
+for i, row in enumerate(grid):
+    for j, col in enumerate(row):
+        # print([i, j])
+        # print(grid[i, j])
+        # get the distance from the point to the populated items
+        dislist = []
+        for item in data:
+            dis = distance([i, j], item)
+            dislist.append([grid[item[1]][item[0]], dis])
+        dislist = np.array(dislist)
+        mindis = min(dislist[:, 1])
+        minitemindex = np.where(dislist[:, 1] == mindis)
+        if np.asarray(minitemindex).shape != (1, 1):
+            continue
+        grid[i, j] = dislist[minitemindex, 0]
+print(grid)
+
+# grid shape
+x, y = grid.shape
+
+# find the largest set of numbers that are contained, and not infinite (not on the edge)
+uniques = np.bincount(grid.flatten())
+ii = np.nonzero(uniques)[0]
+
+# i can replace the array with a single value and compare new found areas with this one
+solutions = []
+for elem, num in zip(ii, uniques[ii]):
+    test = np.where(grid == elem)
+    test = np.array(test)
+    # this test could be better, i think i'm taking a risk here
+    if (test == 0).any() or (test == x-1).any() or (test == y-1).any():
+        # edge case: 
+        # print('{} extends to infinity'.format(elem))
+        continue
+    else:
+        # print('{} IS FINE, size = {}'.format(elem, num))
+        solutions.append([elem, num])
+
+solutions = np.array(solutions)
+print(max(solutions[:, 1]))
+
+""" PART 2"""
+
 # loop over the coordinates again, and for each point fill in another 'ring' around the original point?
 # no, loop over the points and calculate the manhattan distance to all other points. Fill in with the number
 # of the closest point
-print(len(data))
 
 for idx, item in enumerate(data):
     # find all the distances to the other items in data
@@ -64,5 +116,4 @@ for idx, item in enumerate(data):
     else: 
         print("{} is a bad one!".format(grid[item[1]][item[0]]))
 
-# remark: i still need to make the list of all valid areas using the solution for pt1
-# i can probably make this more efficient though, i botched that one pretty badly
+print(solutions)
